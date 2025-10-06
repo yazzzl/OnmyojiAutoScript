5c995c21c97cdfbe8dab6612f25738991d317bdc
yazzzl <looosss3202@gmail.com>
2025-10-07 00:54:50 +0800

    feat: 妖气封印

--- tasks/MonsterSeal/config.py
+++ tasks/MonsterSeal/config.py
@@ -12,6 +12,7 @@ from tasks.Component.config_scheduler import Scheduler
 from tasks.Component.config_base import ConfigBase
 
 
+<<<<<<< HEAD
 class Monster(str, Enum):
     M_TTGG = "跳跳哥哥"
     M_JT = "椒图"
@@ -22,11 +23,29 @@ class Monster(str, Enum):
     M_XSW = "小松丸"
     M_GSH = "鬼使黑"
     M_RHF = "日和坊"
+=======
+# L_NAME = RuleList(folder="./tasks/MonsterSeal/res", direction="vertical", mode="ocr", roi_back=(391,182,198,389), size=(94, 64), array=["跳跳哥哥", "椒图", "骨女", "饿鬼", "二口女", "海坊主", "鬼使黑", "小松丸", "日和坊"])
+
+class Monster(str, Enum):
+    M_TTGG = "ttgg"
+    M_JT = "jt"
+    M_GN = "gn"
+    M_EG = "eg"
+    M_EKN = "ekn"
+    M_HFZ = "hfz"
+    M_XSW = "xsw"
+    M_GSH = "gsh"
+    M_RHF = "rhf"
+>>>>>>> 23382ab (选项优化)
 
 
 class MonsterSealConfig(BaseModel):
    monster_current: Monster = Field(default=Monster.M_GSH, description='target')
+<<<<<<< HEAD
    target :int = Field(default=10, description='count')
+=======
+   target :int = Field(default=10, description='目标次数')
+>>>>>>> 23382ab (选项优化)
 
 
 class MonsterSeal(ConfigBase):
